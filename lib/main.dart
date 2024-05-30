5f9122e2862756c0bb679a9c3d02d7c51d4811e8
Michael Canziani <canziani.michael@gmail.com>
2024-05-30 15:18:06 -0400

    adding log to main

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:putt_putt_pal/util/LoggerUtil.dart';
 import 'pages/LandingPage.dart';
 
 final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 
 void main() {
+  LoggerUtil.info('Application started');
   runApp(
     const ProviderScope(
       child: PuttPuttPal(),
