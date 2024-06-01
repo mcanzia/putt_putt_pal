3f88bc55f2e932713eceec692bc4d513bc6b012e
Michael Canziani <canziani.michael@gmail.com>
2024-05-31 23:31:12 -0400

    updating textbox widths

--- lib/widgets/join_room/JoinRoomSettings.dart
+++ lib/widgets/join_room/JoinRoomSettings.dart
@@ -49,7 +49,7 @@ class _JoinRoomSettingsState extends ConsumerState<JoinRoomSettings> {
   @override
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
-    double buttonWidth = screenWidth * 0.4;
+    double buttonWidth = screenWidth * 0.7;
 
     final PlayerColor playerColor = ref.watch(gameStateProvider).currentColor;
 
--- lib/widgets/waiting_room/HostSettings.dart
+++ lib/widgets/waiting_room/HostSettings.dart
@@ -7,7 +7,7 @@ class HostSettings extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
-    double buttonWidth = screenWidth * 0.4;
+    double buttonWidth = screenWidth * 0.7;
 
     return Container(
       alignment: Alignment.center,
