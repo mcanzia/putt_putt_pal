04a3e978a60208bbb6a6d80ef9ed1fe25c9da8db
Michael Canziani <31520108+mcanzia@users.noreply.github.com>
2024-06-01 17:37:55 -0400

    Update ColorGrid.dart

--- lib/widgets/join_room/ColorGrid.dart
+++ lib/widgets/join_room/ColorGrid.dart
@@ -48,7 +48,7 @@ class _ColorGridState extends ConsumerState<ColorGrid>
       child: CanvasTouchDetector(
         gesturesToOverride: const [GestureType.onTapDown],
         builder: (context) => CustomPaint(
-          size: const Size(350, 350),
+          size: const Size(300, 300),
           painter: ColorGridPainter(
             context: context,
             colors: playerColors,
