728f7029a235897bf0815670396c3015d198a4dd
mcanziani <canziani.developer@gmail.com>
2024-08-16 17:05:18 -0400

    making sure fluttertoast is fully removed

--- lib/util/ExceptionHandler.dart
+++ lib/util/ExceptionHandler.dart
@@ -1,6 +1,5 @@
 import "package:another_flushbar/flushbar.dart";
 import "package:flutter/material.dart";
-import "package:fluttertoast/fluttertoast.dart";
 import "package:putt_putt_pal/router.dart";
 
 class ExceptionHandler {
