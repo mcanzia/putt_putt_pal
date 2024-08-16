468a678773b51438c38da8b8fe00480bc864e0ed
mcanziani <canziani.developer@gmail.com>
2024-08-16 16:58:32 -0400

    trying to fix ios build

--- lib/util/ErrorHandler.dart
+++ lib/util/ErrorHandler.dart
@@ -1,5 +1,4 @@
 import 'package:another_flushbar/flushbar.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 import 'package:flutter/material.dart';
 import 'package:putt_putt_pal/router.dart';
 
